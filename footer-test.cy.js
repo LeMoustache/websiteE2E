de2d0f20036395547d99a38562fa1ceb16bcc6c9
LeMustache <45916929+LeMoustache@users.noreply.github.com>
2023-01-18 10:53:41 +0100

    Add files via upload

---
+++ footer-test.cy.js
@@ -0,0 +1,32 @@
+describe('test footer', () => {
+
+    it('checks bFooter', () => {
+      cy.visit('https://LeMustache.net')
+  
+      cy.get('.bFooter').children()
+        .contains('©2020 LeMustache')
+        
+        .then('check terms links',()=>{
+
+          cy.get('.bFooter').children().children()
+          .contains('Terms')
+          .should('have.attr','href','https://www.lemustache.net/terms')
+          .click()
+          .url('https://www.lemustache.net/terms')
+
+        })
+        
+        .then('check privacy links',()=>{
+
+          cy.visit('https://LeMustache.net')
+          cy.get('.bFooter').children().children()
+          .contains('Privacy')
+          .should('have.attr','href','https://www.lemustache.net/privacy')
+          .click()
+          .url('https://www.lemustache.net/privacy')
+
+        })
+
+    })
+  
+  })
