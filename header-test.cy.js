81c02d1eaf65ce41c2ebd6a7c2f0d2d1c9b25387
LeMustache <45916929+LeMoustache@users.noreply.github.com>
2023-01-18 09:52:45 +0100

    Add files via upload

---
+++ header-test.cy.js
@@ -0,0 +1,37 @@
+describe('test header', () => {
+
+  it('checks Navbar', () => {
+    cy.visit('https://LeMustache.net')
+
+    cy.get('#aboutLink').children()
+      .should('have.attr','href','https://www.lemustache.net/#aboutFrame')
+      .and('contain','About')
+      .click()
+
+    cy.get('#contactLink').children()
+      .should('have.attr','href','https://www.lemustache.net/#contactFrame')
+      .and('contain','Contact')
+      .click()
+
+    cy.get('#projectLink').children()
+      .should('have.attr','href','https://www.lemustache.net/projects')
+      .and('contain','Projects')
+      .click()
+      .url('https://www.lemustache.net/projects')
+  })
+
+  it('checks Topbar left', () => {
+    cy.visit('https://LeMustache.net')
+
+    cy.get('.site-identity')
+      .contains('Le').contains('Mustache')
+
+      .get('#websiteLogoURL')
+      .should('have.attr','href','https://www.lemustache.net')
+      .click({force: true})
+
+      .get('#logo')
+      .should('have.attr','alt','Logo')
+      .and('have.attr','src','../assets/leMoustache_Mustache3.png')
+  })
+})
