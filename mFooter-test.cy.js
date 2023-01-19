aed08b08b04d25244ce10995542d87902f1843e4
LeMustache <eaglehungergamer@gmail.com>
2023-01-19 11:47:27 +0100

    added newsletter test

--- mFooter-test.cy.js
+++ mFooter-test.cy.js
@@ -37,5 +37,69 @@ describe('test footer', () => {
         cy.get('@links').contains('Most Liked Project').click().url('https://www.lemustache.net/projects').mainWebsite()
         cy.get('@links').contains('Other Projects').click().url('https://www.lemustache.net/projects').mainWebsite()
     })
+
+    it('checks email newsletter', () => {
+
+        Cypress.Commands.add('mainWebsite',() => {
+            cy.visit('https://www.lemustache.net')
+        })
+    
+        cy.mainWebsite()
+    
+        const alertShown = cy.stub().as("alertShown")
+        cy.on('window:alert', alertShown)
+    
+        cy.get('.EmailNewslatter').contains('button','Join').as('newsletterButton')
+        cy.get('.EmailNewslatter').get('#emailNewsletterInput').as('newsletterInput')
+    
+        Cypress.Commands.add('clickJoin',() => {
+            cy.get('@newsletterButton').click()
+        })
+    
+        Cypress.Commands.add('showAlert',(message)=>{
+            cy.get("@alertShown").should("have.been", message)
+            cy.on('window:confirm', () => true)
+        })
+    
+        Cypress.Commands.add('enterEmailInput',(email)=>{
+            cy.get("@newsletterInput").type(email)
+        })
+    
+        cy.clickJoin()
+        cy.showAlert('Please Enter A Valid Email')
+    
+        cy.enterEmailInput('e@gmail.com')
+        cy.clickJoin()
+        cy.showAlert('Thank You! A Confirmation Email Has Been Sent')
+    })
+
+    it('checks bFooter', () => {
+        cy.visit('https://LeMustache.net')
+    
+        cy.get('.bFooter').children()
+          .contains('©2020 LeMustache')
+          
+          .then('check terms links',()=>{
+  
+            cy.get('.bFooter').children().children()
+            .contains('Terms')
+            .should('have.attr','href','https://www.lemustache.net/terms')
+            .click()
+            .url().should('equal','https://lemustache.net/terms')
+  
+          })
+          
+          .then('check privacy links',()=>{
+  
+            cy.visit('https://LeMustache.net')
+            cy.get('.bFooter').children().children()
+            .contains('Privacy')
+            .should('have.attr','href','https://www.lemustache.net/privacy')
+            .click()
+            .url().should('equal','https://lemustache.net/privacy')
+  
+          })
+  
+      })
   
   })
